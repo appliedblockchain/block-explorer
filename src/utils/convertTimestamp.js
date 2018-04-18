f0a8e06745f3b0c9ffb7689e043eb532b722954e
godedshaw <ed@appliedblockchain.com>
2018-04-18 15:38:32 +0100

    fix date formats

--- src/utils/convertTimestamp.js
+++ src/utils/convertTimestamp.js
@@ -2,7 +2,7 @@ import format from 'date-fns/format'
 
 export const convertTimestamp = (timeStamp, formatString = 'MMM-DD-YYYY HH:mm A') => {
   if (timeStamp) {
-    const date = new Date(timeStamp)
+    const date = new Date(timeStamp * 1000)
     return format(date, formatString)
   }
 }
